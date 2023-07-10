d55a3d190315fc1131ce31454edb4db8305b28d0
sajal <sajalsahu8@gmail.com>
2023-07-11 02:28:27 +0400

    added screen for meals

---
+++ lib/screens/mealscreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MealScreen extends StatelessWidget {
+  const MealScreen({super.key,});
+
+  @override
+  Widget build(context) {
+    return const Text("sajal");
+  }
+}
