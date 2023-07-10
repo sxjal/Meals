89175de1665f382525463c28976fdeab0bec4e41
sajal <sajalsahu8@gmail.com>
2023-07-11 01:53:44 +0400

    added more data

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,9 +1,7 @@
 import 'package:meals/modal/category.dart';
 import 'package:flutter/material.dart';
- 
- 
-const avaialbleCategories = 
- [
+
+const avaialbleCategories = [
   Category(
     "c1",
     "Italian",
@@ -14,4 +12,44 @@ const avaialbleCategories =
     "Quick & Easy",
     color: Colors.red,
   ),
- ]
+  Category(
+    "c3",
+    "Hamburgers",
+    color: Colors.orange,
+  ),
+  Category(
+    "c4",
+    "German",
+    color: Colors.amber,
+  ),
+  Category(
+    "c5",
+    "Light & Lovely",
+    color: Colors.blue,
+  ),
+  Category(
+    "c6",
+    "Exotic",
+    color: Colors.green,
+  ),
+  Category(
+    "c7",
+    "Breakfast",
+    color: Colors.lightBlue,
+  ),
+  Category(
+    "c8",
+    "Asian",
+    color: Colors.lightGreen,
+  ),
+  Category(
+    "c9",
+    "French",
+    color: Colors.pink,
+  ),
+  Category(
+    "c10",
+    "Summer",
+    color: Colors.teal,
+  ),
+];
