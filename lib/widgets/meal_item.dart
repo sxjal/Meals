5ad8e0fe235559cf7f476f772f173d993ac9885a
sajal <sajalsahu8@gmail.com>
2023-07-11 03:54:55 +0400

    added a few changes to the same

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -18,6 +18,31 @@ class MealItem extends StatelessWidget {
               placeholder: MemoryImage(kTransparentImage),
               image: NetworkImage(meal.imageUrl),
             ),
+            Positioned(
+              bottom: 00,
+              left: 00,
+              right: 50,
+              //top: 10,
+              child: Container(
+                color: Colors.black54,
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    Text(
+                      meal.title,
+                      style: Theme.of(context).textTheme.titleLarge,
+                      maxLines: 2,
+                      textAlign: TextAlign.center,
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    )
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
