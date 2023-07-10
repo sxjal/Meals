5e2f8092831bcb390d9c6de9112a2061a5d02883
sajal <sajalsahu8@gmail.com>
2023-07-11 01:52:48 +0400

    added data and the modal

---
+++ lib/data/data.dart
@@ -0,0 +1,17 @@
+import 'package:meals/modal/category.dart';
+import 'package:flutter/material.dart';
+ 
+ 
+const avaialbleCategories = 
+ [
+  Category(
+    "c1",
+    "Italian",
+    color: Colors.purple,
+  ),
+  Category(
+    "c2",
+    "Quick & Easy",
+    color: Colors.red,
+  ),
+ ]
