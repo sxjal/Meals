234c45ab39988ca55c4a6afb5c3662f8820f1d8a
sajal <sajalsahu8@gmail.com>
2023-07-11 02:36:04 +0400

    configured meals screen

--- lib/screens/mealscreen.dart
+++ lib/screens/mealscreen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:meals/modal/meal.dart';
 
 class MealScreen extends StatelessWidget {
-  const MealScreen({super.key,});
+  const MealScreen({
+    super.key,
+    required this.title,
+    required this.meals,
+  });
+
+  final String title;
+  final List<Meal> meals;
 
   @override
   Widget build(context) {
-    return const Text("sajal");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: ListView(children: [
+          ...meals.map((meal) {
+            return ListTile(
+              title: Text(meal.title),
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(meal.imageUrl),
+              ),
+            );
+          }).toList(),
+        ]),
+      ),
+    );
   }
 }
