a60da3dba771190cf778b00310a43387cd3c6e8e
sajal <sajalsahu8@gmail.com>
2023-07-12 03:07:24 +0400

    configured alot of things

--- lib/screens/MealSpecific.dart
+++ lib/screens/MealSpecific.dart
@@ -9,35 +9,85 @@ class MealSpecific extends StatelessWidget {
   Widget build(context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Receipe"),
+        title: const Text("Receipe"),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-            child: Container(
-              height: 400,
-              child: Card(
-                //padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                //decoration: BoxDecoration(
-                //borderRadius: BorderRadius.circular(20),
-                //),
-                //width: double.infinity,
-                //height: 400,
-                elevation: 20,
-                
-                child: Image.network(
-                  meal.imageUrl,
-                  fit: BoxFit.cover,
-                ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+              padding: const EdgeInsets.fromLTRB(30, 5, 30, 20),
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(182, 67, 96, 100),
+              ),
+              child: Stack(
+                fit: StackFit.expand,
+                //clipBehavior: Clip.antiAliasWithSaveLayer,
+                children: [
+                  Card(
+                    elevation: 30,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10.0),
+                      child: Image.network(
+                        meal.imageUrl,
+                        fit: BoxFit.cover,
+                        //width: 20,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    child: Expanded(
+                      child: Column(
+                        children: [
+                          Text(
+                            meal.title,
+                            style: Theme.of(context).textTheme.titleLarge,
+                          ),
+                          Row(
+                            children: [
+                              const Spacer(),
+                              Icon(
+                                Icons.bento,
+                                color: meal.isLactoseFree
+                                    ? Colors.greenAccent
+                                    : Colors.redAccent,
+                              ),
+                              const Spacer(),
+                              Icon(
+                                Icons.smoke_free,
+                                color: meal.isGlutenFree
+                                    ? Colors.greenAccent
+                                    : Colors.redAccent,
+                              ),
+                              const Spacer(),
+                              Icon(
+                                Icons.bento,
+                                color: meal.isVegan
+                                    ? Colors.greenAccent
+                                    : Colors.redAccent,
+                              ),
+                              const Spacer(),
+                              Icon(
+                                Icons.smoke_free,
+                                color: meal.isVegetarian
+                                    ? Colors.greenAccent
+                                    : Colors.redAccent,
+                              ),
+                              const Spacer(),
+                              // Icon(icon),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-          Text(
-            meal.title,
-            style: Theme.of(context).textTheme.titleLarge,
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
