cb7b77706d333074d337e6a4da9ff52445c1eee5
sajal <sajalsahu8@gmail.com>
2023-07-12 18:22:28 +0400

    added new screen for tabs

---
+++ lib/screens/tabs.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Tabs extends StatefulWidget {
+  const Tabs({super.key});
+
+  @override
+  State<Tabs> createState() => _TabsState();
+}
+
+class _TabsState extends State<Tabs> {
+  @override
+  Widget build(context) {
+    return Text("Tabs");
+  }
+}
