0769bdd8f6aefea9b5937a7c91334ccbb58dc8f4
sajal <sajalsahu8@gmail.com>
2023-07-13 01:36:45 +0400

    added decoration to the drawer

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -5,6 +5,39 @@ class MainDrawer extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return const Drawer();
+    return Drawer(
+      elevation: 10,
+      child: Column(
+        children: [
+          DrawerHeader(
+            padding: const EdgeInsets.all(20),
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.black12,
+                  Colors.black26,
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: Row(
+              children: [
+                const Icon(
+                  Icons.fastfood,
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  "Cooking Up!",
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
