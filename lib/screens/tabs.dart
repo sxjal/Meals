aaebfa98568ead77eb452787d4c696c5f306441e
sajal <sajalsahu8@gmail.com>
2023-07-12 18:40:34 +0400

    added bottomnavbar

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals/screens/categories.dart';
+import 'package:meals/screens/mealscreen.dart';
 
 class Tabs extends StatefulWidget {
   const Tabs({super.key});
@@ -8,8 +10,44 @@ class Tabs extends StatefulWidget {
 }
 
 class _TabsState extends State<Tabs> {
+  int _selectedpageindex = 0;
+
+  void _selectpage(int index) {
+    setState(() {
+      _selectedpageindex = index;
+    });
+  }
+
   @override
   Widget build(context) {
-    return Text("Tabs");
+    Widget activestate = const CategoriesScreen();
+    var activepagetitle = "Categories";
+    if (_selectedpageindex == 1) {
+      activestate = const MealScreen(title: "Favorites", meals: []);
+      activepagetitle = "Favorites";
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("sajal"),
+      ),
+      body: activestate,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectpage,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.set_meal,
+            ),
+            label: "Categories",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.star,
+            ),
+            label: "Favorites",
+          ),
+        ],
+      ),
+    );
   }
 }
